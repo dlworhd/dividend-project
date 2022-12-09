e2775af91d6f81abdcdda1f611877a38ab76edca
dlwodud821@gmail.com <dlwodud821@gmail.com>
2022-12-10 02:16:03 +0900

    init

---
+++ src/test/java/com/zerobase/bdg/BdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.bdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
